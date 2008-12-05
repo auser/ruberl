379d14d8bd2fb94b27fefcdfcfabc6e3eca3b5ec
Ari Lerner <ari.lerner@citrusbyte.com>
2008-12-04 17:27:04 -0800

    Initial commit

---
+++ lib/ruberl.rb
@@ -0,0 +1,31 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module Ruberl
+  attr_accessor :host, :port
+  def initialize(host="localhost", port=7050)
+    @host = host
+    @port = port
+  end
+  def with_socket(&block)
+    begin
+      socket = TCPSocket.open(@host, @port)
+      out = yield(socket)
+      socket.close
+      out
+    rescue Exception => e
+    end
+  end
+  def messenger_send!(msg="get_current_load cpu")
+    with_socket do |sock|
+      sock.send(msg, 0)
+      @str = sock.recv(2000)      
+    end
+    @str
+  end
+  def messenger_cast!(msg="force_reconfig")
+    with_socket do |sock|
+      sock.send(msg, 0)
+    end
+  end
+end
---
+++ test/test_ruberl.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestRuberl < Test::Unit::TestCase
+
+  def setup
+  end
+  
+  def test_truth
+    assert true
+  end
+end
