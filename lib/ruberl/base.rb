6af4ca0e4da75879d8da19d737e214c7fb1006fb
Ari Lerner <alerner@ypcmc05591.yellowpages.local>
2009-03-03 11:58:56 -0800

    Updated gem with messager_cast for udp calls

--- lib/ruberl/base.rb
+++ lib/ruberl/base.rb
@@ -1,9 +1,11 @@
 module Ruberl
   class Base
-    attr_accessor :host, :port
+    attr_accessor :host, :port, :recv_timeout
     def initialize(host="localhost", port=7050)
       @host = host
       @port = port
+      
+      @recv_timeout = 3
     end
     def with_socket(&block)
       begin
@@ -14,6 +16,27 @@ module Ruberl
       rescue Exception => e
       end
     end
+    def with_udp(&block)
+      out,sock = nil,nil
+      begin
+        socket = UDPSocket.open
+        out = yield(socket)
+      rescue IOError, SystemCallError
+      ensure
+        socket.close if socket
+      end
+      out
+    end
+    def messenger_cast!(cmd)
+      with_udp do |sock|
+        sock.send(cmd, 0, @host, @port)
+        @resp = if select([sock], nil, nil, @recv_timeout)
+          sock.recvfrom(65536)
+        end
+        @resp[0] = @resp[0][4..-1] if @resp
+      end
+      @resp ? @resp[0] : nil
+    end
     def messenger_send!(msg="get_current_load cpu")
       with_socket do |sock|
         sock.send(msg, 0)
@@ -21,10 +44,10 @@ module Ruberl
       end
       @str
     end
-    def messenger_cast!(msg="force_reconfig")
-      with_socket do |sock|
-        sock.send(msg, 0)
-      end
-    end
+    # def messenger_cast!(msg="force_reconfig")
+    #   with_socket do |sock|
+    #     sock.send(msg, 0)
+    #   end
+    # end
   end
 end
